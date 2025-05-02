5c3b9949e745e56bc48a51a34ae186f8b6a962f5
edwardmontclair <raymundoe1@montclair.edu>
2025-05-01 23:31:31 -0400

    Update create_db.py

--- University Library Project/create_db.py
+++ University Library Project/create_db.py
@@ -1,18 +1,17 @@
 import sqlite3
 
 conn = sqlite3.connect('users.db')
-
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users
-    (
+    CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         username TEXT NOT NULL UNIQUE,
-        password TEXT NOT NULL
+        password TEXT NOT NULL,
+        role TEXT DEFAULT 'user',
+        two_fa_enabled INTEGER DEFAULT 0
     )
 ''')
 
 conn.commit()
-
 conn.close()
