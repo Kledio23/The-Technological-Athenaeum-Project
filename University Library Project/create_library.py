f2244b1c24857a6ce47c0d58ad67af862cbd53eb
edwardmontclair <raymundoe1@montclair.edu>
2025-05-01 23:32:36 -0400

    Update create_library.py

--- University Library Project/create_library.py
+++ University Library Project/create_library.py
@@ -4,15 +4,14 @@ conn = sqlite3.connect('library.db')
 cursor = conn.cursor()
 
 cursor.execute('''
-    CREATE TABLE IF NOT EXISTS books
-    (
+    CREATE TABLE IF NOT EXISTS books (
         isbn TEXT PRIMARY KEY,
         title TEXT NOT NULL,
         author TEXT NOT NULL,
-        availability TEXT NOT NULL DEFAULT 'available'
+        availability TEXT NOT NULL,
+        reserved_by TEXT
     )
-''')
+    ''')
 
 conn.commit()
-
 conn.close()
